b8a4eb669f8c4ff552206918f8bc2151068b1ae1
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2022-10-30 20:24:51 +1100

    Add bucket with random name

--- main.tf
+++ main.tf
@@ -2,15 +2,35 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.37.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region = var.region
 }
 
+resource "random_pet" "lambda_bucket_name" {
+  prefix = var.project
+  length = 2
+}
 
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "AWS Region"
+  default     = "ap-southeast-2"
+}
+
+variable "project" {
+  type        = string
+  description = "Project name"
+  default     = "le.onl"
+}
